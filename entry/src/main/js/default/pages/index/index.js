8ad988652b55191ffa93072ec4e7833d90023d87
liuchenyang0515 <897418643@qq.com>
2020-09-24 15:28:40 +0800

    step6 将主页面中picker选择的值传递到第二个页面中

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,5 +1,8 @@
 import router from '@system.router';
 
+let pickerValue1 = null;
+let pickerValue2 = null;
+
 export default {
   data: {
     picker1range: ["1", "2", "3"],
@@ -7,14 +10,20 @@ export default {
   },
   clickAction() {
     router.replace({
-      uri:"pages/xunlian/xunlian"
+      uri: "pages/xunlian/xunlian",
+      params: { // 传递到目标页面的参数
+        "data1": pickerValue1,
+        "data2": pickerValue2
+      }
     })
   },
   changeAction1(pv) {
     console.log("左边的选中项:" + pv.newValue);
+    pickerValue1 = pv.newValue;
   },
   changeAction2(pv) {
     console.log(`右边的选中项:${pv.newValue}`); // 说明ES6也是支持的
+    pickerValue2 = pv.newValue;
   },
   onInit() {
     console.log("主页面的onInit()被调用");
