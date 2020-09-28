f04ede6392646afef876e2e63ae37de1b1795862
liuchenyang0515 <897418643@qq.com>
2020-09-28 17:17:18 +0800

    step14 从主页跳转到3s倒计时页面，最后停在倒计时页面

--- entry/src/main/js/default/pages/daojishi/daojishi.js
+++ entry/src/main/js/default/pages/daojishi/daojishi.js
@@ -1,4 +1,29 @@
+let counter = 3;
+let timer = null;
 export default {
     data: {
+        imgSrc: "",
+        seconds: ""
+    },
+    run() {
+        --counter;
+        if (counter) {
+            this.imgSrc = "/common/" + counter + ".png";
+            this.seconds = counter.toString();
+        } else {
+            this.imgSrc = "";
+            this.seconds = "";
+
+            clearInterval(timer);
+            timer = null;
+        }
+    },
+    onInit() {
+        this.imgSrc = "/common/" + counter + ".png";
+        console.log(this.imgSrc);
+        this.seconds = counter.toString();
+    },
+    onShow() {
+        timer = setInterval(this.run, 1000);
     }
 }
