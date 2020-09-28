af5cc62b584b8d2aa7334a5a71936fbf53e1f2bf
liuchenyang0515 <897418643@qq.com>
2020-09-28 19:02:41 +0800

    step15 3s倒计时完成后跳转到训练页面并传递数据，添加最后的READEME.md

--- entry/src/main/js/default/pages/daojishi/daojishi.js
+++ entry/src/main/js/default/pages/daojishi/daojishi.js
@@ -1,5 +1,10 @@
+import router from '@system.router';
+
 let counter = 3;
 let timer = null;
+let pv1 = null;
+let pv2 = null;
+
 export default {
     data: {
         imgSrc: "",
@@ -16,9 +21,18 @@ export default {
 
             clearInterval(timer);
             timer = null;
+            router.replace({
+                uri: 'pages/xunlian/xunlian',
+                params: {
+                    "key1": pv1,
+                    "key2": pv2
+                }
+            });
         }
     },
     onInit() {
+        pv1 = this.data1;
+        pv2 = this.data2;
         this.imgSrc = "/common/" + counter + ".png";
         console.log(this.imgSrc);
         this.seconds = counter.toString();
